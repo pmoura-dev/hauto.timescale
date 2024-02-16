635373f1c4c45e7af65fb895f61d98576a7e177a
Pedro Moura <pmoura.dev@gmail.com>
2024-02-16 23:40:39 +0000

    add get_device_state function

--- scripts/functions/install.sh
+++ scripts/functions/install.sh
@@ -2,5 +2,6 @@
 
 echo "INSTALLING FUNCTIONS..."
 
-psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -a -f get_devices_mqtt_configuration.sql
+psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -a -f get_device_state.sql
 psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -a -f upsert_device_status.sql
+psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -a -f get_devices_mqtt_configuration.sql
